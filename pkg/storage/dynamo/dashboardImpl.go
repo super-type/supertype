fb9652c6dd6961f10cb68cbefaaa582720b6c024
Carter Klein <carterklein13@gmail.com>
2021-01-08 18:41:51 -0500

    Adding Uber zap leveled logging

--- pkg/storage/dynamo/dashboardImpl.go
+++ pkg/storage/dynamo/dashboardImpl.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"go.uber.org/zap"
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/fatih/color"
 	"github.com/super-type/supertype/internal/utils"
 	"github.com/super-type/supertype/pkg/authenticating"
 	"github.com/super-type/supertype/pkg/dashboard"
@@ -19,6 +19,7 @@ import (
 
 // ListAttributes returns all observations in the Supertype ecosystem
 func (d *Storage) ListAttributes() ([]string, error) {
+	zap.S().Info("Listing attributes...")
 	// Initialize AWS session
 	svc := utils.SetupAWSSession()
 	input := &dynamodb.ListTablesInput{}
@@ -30,10 +31,10 @@ func (d *Storage) ListAttributes() ([]string, error) {
 		result, err := svc.ListTables(input)
 		if err != nil {
 			if _, ok := err.(awserr.Error); ok {
-				color.Red("Dynamo internal server error")
+				zap.S().Error("Dynamo internal server error")
 				return nil, dashboard.ErrDynamoInternalError
 			}
-			color.Red("Dynamo error")
+			zap.S().Errorf("Dynamo error: %v", err)
 			return nil, dashboard.ErrDynamoError
 		}
 
@@ -53,20 +54,23 @@ func (d *Storage) ListAttributes() ([]string, error) {
 		}
 	}
 
+	zap.S().Info("Successfully retrieved attributes")
 	return response, nil
 }
 
 // RegisterWebhook creates a new webhook on a vendor's request
 func (d *Storage) RegisterWebhook(webhookRequest dashboard.WebhookRequest, apiKey string) error {
+	zap.S().Info("Registering webhook...")
 	apiKeyHash := utils.GetAPIKeyHash(apiKey)
 	databaseAPIKeyHash, err := ScanDynamoDBWithKeyCondition("vendor", "apiKeyHash", "apiKeyHash", apiKeyHash)
 	if err != nil || databaseAPIKeyHash == nil {
+		zap.S().Errorf("Error getting vendor's public key given API Key: %v", err)
 		return err
 	}
 
 	// Compare requesting API Key with our internal API Key. If they don't match, it's not coming from the vendor
 	if *databaseAPIKeyHash != apiKeyHash {
-		color.Red("!!! Vendor secret key hashes do no match - potential malicious attempt !!!")
+		zap.S().Errorf("Stored API hash does not match given (%s) - potential malicious attempt!", apiKeyHash)
 		return storage.ErrAPIKeyDoesNotMatch
 	}
 
@@ -89,11 +93,13 @@ func (d *Storage) RegisterWebhook(webhookRequest dashboard.WebhookRequest, apiKe
 	// Get attribute from subscribers
 	result, err := GetItemDynamoDB(svc, "subscribers", "attribute", destination[0])
 	if err != nil {
+		zap.S().Errorf("Failed to get subscribers to %v from DynamoDB: %v", destination[0], err)
 		return err
 	}
 	var levels interface{}
 	err = dynamodbattribute.UnmarshalMap(result.Item, &levels)
 	if err != nil {
+		zap.S().Errorf("Error unmarshaling levels: %v", err)
 		return err
 	}
 
@@ -102,35 +108,39 @@ func (d *Storage) RegisterWebhook(webhookRequest dashboard.WebhookRequest, apiKe
 		var attribute dashboard.MasterBedroom
 		err = dynamodbattribute.UnmarshalMap(result.Item, &attribute)
 		if err != nil {
+			zap.S().Errorf("Error unmarshaling attribute: %v", err)
 			return err
 		}
 
 		b, err := json.Marshal(attribute)
 		if err != nil {
+			zap.S().Errorf("Error encoding request: %v", err)
 			return err
 		}
 
 		err = utils.ValidateNewSubscriberURL(string(b), webhookRequest.Endpoint)
 		if err != nil {
+			zap.S().Errorf("Error validating endpoint %v : %v", webhookRequest.Endpoint, err)
 			return err
 		}
 
 		urls := GetSubscribersFromEndpoint(destination, levels)
 		updatedAttribute, err := utils.AppendToSubscribers(string(b), urls, webhookRequest.Endpoint)
 		if err != nil {
+			zap.S().Errorf("Error appending to endpoint: %v", err)
 			return err
 		}
 
 		resp := dashboard.MasterBedroom{}
 		err = json.Unmarshal([]byte(*updatedAttribute), &resp)
 		if err != nil {
-			fmt.Println(err)
+			zap.S().Errorf("Error unmarshaling attribute %s : %v", *updatedAttribute, err)
 			return err
 		}
 
 		err = PutItemInDynamoDB(resp, "subscribers", svc)
 		if err != nil {
-			fmt.Println(err)
+			zap.S().Errorf("Error putting item %s in DynamoDB: %v", fmt.Sprint(resp), err)
 			return err
 		}
 	// This could get ugly, fast... we should try to think of a way to do this more programmatically
@@ -142,22 +152,25 @@ func (d *Storage) RegisterWebhook(webhookRequest dashboard.WebhookRequest, apiKe
 	case "garage":
 	case "bathroom":
 	default:
+		zap.S().Errorf("Invalid attribute %s : %v", destination[0])
 		return errors.New("Invalid attribute")
 	}
 
 	username, err := ScanDynamoDBWithKeyCondition("vendor", "username", "apiKeyHash", apiKeyHash)
 	if err != nil {
+		zap.S().Errorf("Username already exists: %v", err)
 		return err
 	}
 	result, err = GetItemDynamoDB(svc, "vendor", "username", *username)
 	if err != nil {
+		zap.S().Errorf("Failed to get vendor from DynamoDB: %v", err)
 		return err
 	}
 
 	vendor := authenticating.CreateVendor{}
 	err = dynamodbattribute.UnmarshalMap(result.Item, &vendor)
 	if err != nil {
-		color.Red("Error unmarshaling data")
+		zap.S().Errorf("Error unmarshaling vendor: %v", err)
 		return storage.ErrUnmarshaling
 	}
 
@@ -180,9 +193,10 @@ func (d *Storage) RegisterWebhook(webhookRequest dashboard.WebhookRequest, apiKe
 	// The URL can't exist in one place but not the other... maybe this is bad practice without a single source of truth?
 	err = PutItemInDynamoDB(updatedVendor, "vendor", svc)
 	if err != nil {
-		fmt.Println(err)
+		zap.S().Errorf("Error putting vendor %s in DynamoDB: %v", fmt.Sprint(updatedVendor), err)
 		return err
 	}
 
+	zap.S().Info("Successfully registered webhook!")
 	return nil
 }
